9ba09a533e739de3ac30f7c8406a8cd54a689785
Harsh Gaur <21bcs2733@cuchd.in>
2025-10-04 20:40:55 +0530

    remove unused invite, message, and stream session controllers and models; update room controller and routes for invite creation and participant role management

--- src/routes/room.route.js
+++ src/routes/room.route.js
@@ -1,6 +1,6 @@
 import {Router} from 'express';
 import { verifyJWT } from '../middlewares/auth.middleware.js';
-import { createRoom, endRoom, getRoomHistory, joinRoom, leaveRoom, deleteAllRoomsHistory, updateRoomSettings, updateParticipantAccess } from '../controllers/room.controller.js';
+import { createRoom, endRoom, getRoomHistory, joinRoom, leaveRoom, deleteAllRoomsHistory, updateRoomSettings, updateParticipantRole } from '../controllers/room.controller.js';
 
 const router = Router();
 
@@ -11,6 +11,6 @@ router.route("/:roomCode/end").patch(verifyJWT, endRoom);
 router.route("/history").get(verifyJWT, getRoomHistory);
 router.route("/history/clear").delete(verifyJWT, deleteAllRoomsHistory);
 router.route("/:roomCode/settings").patch(verifyJWT, updateRoomSettings); 
-router.route("/:roomCode/participant/:participantId").patch(verifyJWT, updateParticipantAccess);
+router.route("/:roomCode/participant/:participantId").patch(verifyJWT, updateParticipantRole);
 
 export default router;
