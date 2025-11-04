51b8e6cd524eed5a35a77fc1875574b2f595bc22
Harsh Gaur <21bcs2733@cuchd.in>
2025-11-05 01:45:01 +0530

    change the json limit

--- src/app.js
+++ src/app.js
@@ -9,10 +9,10 @@ app.use(cors({
     credentials: true,
 }));
 
-app.use(express.json({limit: "16kb"}));
+app.use(express.json({limit: "1mb"}));
 app.use(express.urlencoded({
     extended: true,
-    limit: "16kb"
+    limit: "1mb"
 }));
 app.use(express.static("public"));
 app.use(cookieParser());
