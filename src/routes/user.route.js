6202de40e99666e150a7089c1ba35773bcf5290b
Harsh Gaur <21bcs2733@cuchd.in>
2025-11-09 00:38:18 +0530

    remove getUserProfile endpoint and replace with getLoggedInUserAccessToken in user routes

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { loginUser, logoutUser, refreshAccessToken, registerUser, getUserProfile } from "../controllers/user.controller.js";
+import { loginUser, logoutUser, refreshAccessToken, registerUser, getLoggedInUserAccessToken } from "../controllers/user.controller.js";
 import {upload} from "../middlewares/multer.middleware.js"
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
@@ -8,9 +8,9 @@ const router = Router();
 router.route("/register").post(upload.single("avatar"), registerUser);
 router.route("/login").post(loginUser);
 
-router.route("/profile").get(verifyJWT, getUserProfile);
-
 router.route("/logout").post(verifyJWT ,logoutUser);
 router.route("/renewToken").post(refreshAccessToken);
+
+router.route("/auth/me").get(verifyJWT, getLoggedInUserAccessToken);
 
 export default router;
